253bf2426ec1a5a26de3b65581421a86a416a8b5
Sonia Hussain <sonia_ahm@icloud.com>
2023-05-31 11:38:20 +0100

    Added validations to restaurant and review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  # when a restaurant is destroyed, all reviews will be destroyed as well
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  # category has a fixed list
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@
 
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  # must have a rating between 0 and 5 and must be a num. not 2.5
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
